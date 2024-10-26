63e95bd54bd4d0552845c847c35653478107f446
Kaya-Mehmet <Kaya.Mehmet@city.ac.uk>
2024-10-26 21:52:33 +0100

    HW 4

---
+++ HW 4/task2.sql
@@ -0,0 +1,22 @@
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
+VALUES ('Fluffy', '2020-10-15', 'vet', 'antibiotics');
+
+INSERT INTO petPet (petname, owner, species, gender, birth)
+VALUES ('Hammy', 'Diane', 'hamster', 'M', '2010-10-30');
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
+VALUES ('Hammy', '2020-10-15', 'vet', 'antibiotics');
+
+UPDATE petEvent
+SET remark = '5 kittens, 2 male'
+WHERE petname = 'Fluffy' AND eventtype = 'litter';
+
+UPDATE petEvent
+SET petname = 'Claws'
+WHERE petname = 'Slim' AND eventdate = '1997-08-03';
+
+UPDATE petPet
+SET death = '2020-09-01'
+WHERE petname = 'Puffball';
+
+DELETE FROM petPet
+WHERE owner = 'Harold';
